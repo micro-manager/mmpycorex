b466d97c1891b57f03eb1d893e573414327e3f0e
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2024-07-13 17:03:18 +0200

    initial commit

---
+++ src/mmpycorex/__init__.py
@@ -0,0 +1,8 @@
+"""
+mmpycorex package
+
+This package provides access to pymmcore and the ZeroMQ wrapper to MMCoreJ,
+along with associated utilities for downloading and running headless.
+"""
+
+from ._version import __version__
